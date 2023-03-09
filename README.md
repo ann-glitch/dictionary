559e4bc542feea4279bb4c8ff6600b2bbb7cddc5
ann <dorisann.me@gmail.com>
2023-03-09 10:24:19 +0000

    add: readme file and finishing touches

---
+++ README.md
@@ -0,0 +1,14 @@
+# interactive_dictionary
+
+> a python interactive dictionary that accepts a user word input and returns the word's definition. It is a command line user interface.
+
+## functionalities
+
+- accepts a user word input and returns the word's definition(s).
+- it fixes misspelled words.
+
+## run
+
+```python
+    python3 app.py
+```
--- app.py
+++ app.py
@@ -4,12 +4,24 @@ from difflib import get_close_matches
 # load json data 
 my_data = json.load(open("data.json"))
 
-def translate(word):
+def wordfinder(word):
     word = word.lower()
+
     if word in my_data:
         return my_data[word]
+
     elif len(get_close_matches(word, my_data.keys())) > 0: 
-        return input("Sorry did you mean {} instead? Enter Y if yes, or N if no.".format(get_close_matches(word, my_data.keys())[0]))
+        entry = input("Sorry did you mean {} instead? Enter Y if yes, or N if no: c".format(get_close_matches(word, my_data.keys())[0]))
+
+        if entry == "Y":
+            return my_data[get_close_matches(word, my_data.keys())[0]]
+
+        elif entry == "N":
+            return "Word not found, please double check."  
+
+        else:
+            return "Sorry, we did not understand your input."  
+
     else:
         return "Word not found, please double check."    
     
@@ -17,4 +29,10 @@ def translate(word):
 word = input("Please enter a word you'd like to know about: ")
 
 
-print(translate(word))
+output = wordfinder(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+        print(output)    
